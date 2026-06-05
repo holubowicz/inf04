776f7f03d90dfa9e04cf5d31c76deab669a8c24f
Tymoteusz Hołubowicz <103210917+holubowicz@users.noreply.github.com>
2026-06-05 22:28:04 +0200

    feat: add new exam solution 2021_06_01

---
+++ inf04_2021_06_01/rozwiazanie/konsola/ConsoleApp/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        private static readonly int _arraySize = 10;
+        private static int[] _array = new int[_arraySize];
+        
+        internal static void Main(string[] args)
+        {
+            GetUserInput();
+            SelectionSort();
+            PrintArray();
+        }
+
+        /********************************************************
+        * nazwa funkcji: SelectionSort
+        * parametry wejściowe: brak
+        * wartość zwracana: brak
+        * autor: 00000000000
+        * ****************************************************/
+        private static void SelectionSort()
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                int maxIndex = FindMaxIndex(i);
+                
+                int temp = _array[i];
+                _array[i] = _array[maxIndex];
+                _array[maxIndex] = temp;
+            }
+        }
+
+        /********************************************************
+         * nazwa funkcji: FindMaxIndex
+         * parametry wejściowe: startIndex - początkowy indeks wyszukiwania maksymalnej liczby
+         * wartość zwracana: brak
+         * autor: 00000000000
+         * ****************************************************/
+        private static int FindMaxIndex(int startIndex)
+        {
+            int maxIndex = startIndex;
+            for (int i = startIndex + 1; i < _array.Length; i++)
+            {
+                if (_array[i] > _array[maxIndex])
+                {
+                    maxIndex = i;
+                }
+            }
+            return maxIndex;
+        }
+
+        private static void GetUserInput()
+        {
+            Console.WriteLine($"Podaj tablice liczb całkowitych");
+
+            for (int i = 0; i < _arraySize; i++)
+            {
+                while (true)
+                {
+                    Console.Write($"Podaj {i + 1} liczbe: ");
+                    if (int.TryParse(Console.ReadLine(), out int number))
+                    {
+                        _array[i] = number;
+                        break;
+                    }
+                }
+            }
+        }
+
+        private static void PrintArray()
+        {
+            Console.WriteLine("\nPosortowana tablica:");
+            for (int i = 0; i < _arraySize; i++)
+            {
+                Console.WriteLine($"{i + 1}. {_array[i]}");
+            }
+        }
+    }
+}
---
+++ inf04_2021_06_01/rozwiazanie/mobilna/MobileApp/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace MobileApp;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void ConfirmButton_OnClicked(object? sender, EventArgs e)
+    {
+        string email = EmailEntry.Text;
+        string password = PasswordEntry.Text;
+        string passwordRepetition = PasswordRepetitionEntry.Text;
+
+        if (string.IsNullOrEmpty(email) || !email.Contains("@"))
+        {
+            MessageLabel.Text = "Nieprawidłowy adres e-mail";
+            return;
+        }
+
+        if (password != passwordRepetition)
+        {
+            MessageLabel.Text = "Hasła się różnią";
+            return;
+        }
+
+        MessageLabel.Text = $"Witaj {email}";
+    }
+}
