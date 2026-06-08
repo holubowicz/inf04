add425a47b5a6573df36025c4adf90a725a992e2
Tymoteusz Hołubowicz <103210917+holubowicz@users.noreply.github.com>
2026-06-08 23:55:26 +0200

    feat: add solution for 2023_06_01 exam

---
+++ inf04_2023_06_01/rozwiazanie/desktop/DesktopApp/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            UpdateShipmentType();
+        }
+
+        private void CheckPriceButton_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateShipmentType();
+        }
+
+        private void ConfirmButton_Click(object sender, RoutedEventArgs e)
+        {
+            string zipCode = ZipCodeTextBox.Text;
+
+            if (zipCode.Length != 5)
+            {
+                MessageBox.Show(
+                    "Nieprawidłowa liczba cyfr w kodzie pocztowym",
+                    "Błąd",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+                return;
+            }
+
+            if (!int.TryParse(zipCode, out int _))
+            {
+                MessageBox.Show(
+                    "Kod pocztowy powinien się składać z samych cyfr",
+                    "Błąd",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+                return;
+            }
+
+            MessageBox.Show("Dane przesyłki zostały wprowadzone",
+                "Informacja",
+                MessageBoxButton.OK,
+                MessageBoxImage.Information);
+        }
+
+        private void UpdateShipmentType()
+        {
+            string imageName = "pocztowka";
+            double price = 1;
+
+            if ((bool)LetterRadioButton.IsChecked)
+            {
+                imageName = "list";
+                price = 1.5;
+            }
+            else if ((bool)PackageRadioButton.IsChecked)
+            {
+                imageName = "paczka";
+                price = 10;
+            }
+
+            ShipmentTypeImage.Source = new BitmapImage(new Uri($"/Images/{imageName}.png", UriKind.Relative));
+            ShipmentPriceLabel.Content = $"Cena: {price.ToString(new CultureInfo("pl"))} zł";
+        }
+    }
+}
---
+++ inf04_2023_06_01/rozwiazanie/konsola/ConsoleApp/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        private static readonly int RangeStart = 2;
+        private static readonly int RangeEnd = 100;
+
+        internal static void Main(string[] args)
+        {
+            bool[] array = new bool[RangeEnd + 1];
+            PopulateArray(array);
+
+            int n = (int)Math.Sqrt(RangeEnd);
+            for (int i = RangeStart; i <= n; i++)
+            {
+                if (!array[i])
+                {
+                    continue;
+                }
+
+                for (int j = RangeStart * i; j <= RangeEnd; j += i)
+                {
+                    array[j] = false;
+                }
+            }
+            
+            Console.WriteLine("Wszystkie liczby pierwsze:");
+            for (int i = RangeStart; i <= RangeEnd; i++)
+            {
+                if (array[i])
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+
+        /*******************************************************
+         nazwa funkcji:       PopulateArray
+         parametry wejściowe: array - tablica wartości typu logicznego, czy indeks jest liczbą pierwszą
+         wartość zwracana:    brak
+         informacje:          Wypełnia tablicę wartościami true, od indeksu 2 do 100, co oznacza, że te liczby
+                              mogą być pierwsze.
+         autor:               00000000000
+        ****************************************************/
+        private static void PopulateArray(bool[] array)
+        {
+            for (int i = RangeStart; i <= RangeEnd; i++)
+            {
+                array[i] = true;
+            }
+        }
+    }
+}
