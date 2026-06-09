d6c62f9570b0a1575f6750e8b0b962209bf4543b
Tymoteusz Hołubowicz <103210917+holubowicz@users.noreply.github.com>
2026-06-09 19:10:18 +0200

    feat: add solution for 2023_06_02 exam

---
+++ inf04_2023_06_02/rozwiazanie/konsola/ConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        private static readonly int RangeStart = 0;
+        private static readonly int RangeEnd = 1000;
+        private static readonly int ArraySize = 100;
+
+        private static readonly Random Rand = new Random();
+
+        internal static void Main(string[] args)
+        {
+            int[] array = new int[ArraySize];
+            PopulateArray(array);
+
+            BubbleSort(array);
+
+            Console.WriteLine("Posortowana tablica liczb całkowitych:");
+            Console.WriteLine(string.Join(", ", array));
+        }
+
+        private static void PopulateArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Rand.Next(RangeStart, RangeEnd + 1);
+            }
+        }
+
+        private static void BubbleSort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ inf04_2023_06_02/rozwiazanie/mobilna/MobileApp/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿namespace MobileApp;
+
+public partial class MainPage : ContentPage
+{
+    private static readonly string[] Quotes = new string[]
+    {
+        "Dzień dobry",
+        "Good morning",
+        "Buenos dias"
+    };
+    
+    private int _quoteIndex;
+    
+    public MainPage()
+    {
+        InitializeComponent();
+        UpdateQuoteLabel();
+    }
+
+    private void SizeSlider_OnValueChanged(object? sender, ValueChangedEventArgs e)
+    {
+        Slider slider = (Slider)sender!;
+        int fontSize = (int)slider.Value;
+        
+        QuoteLabel.FontSize = fontSize;
+        SizeLabel.Text = $"Rozmiar: {fontSize}";
+    }
+    
+    private void NextButton_OnClicked(object? sender, EventArgs e)
+    {
+        _quoteIndex++;
+        UpdateQuoteLabel();
+    }
+
+    private void UpdateQuoteLabel()
+    {
+        _quoteIndex %= Quotes.Length;
+        QuoteLabel.Text = Quotes[_quoteIndex];
+    }
+}
