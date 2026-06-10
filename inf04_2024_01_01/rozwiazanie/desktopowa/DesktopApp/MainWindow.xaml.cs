41faaabd0705a77ccfad2aa716c201a67f385d93
Tymoteusz Hołubowicz <103210917+holubowicz@users.noreply.github.com>
2026-06-10 14:13:36 +0200

    feat: add solution for 2024_01_01 exam

---
+++ inf04_2024_01_01/rozwiazanie/desktopowa/DesktopApp/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string _selectedEyeColor = "niebieskie";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NumberTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            string number = textBox.Text;
+
+            if (number != "000" && number != "111" && number != "333")
+            {
+                PersonImage.Visibility = Visibility.Collapsed;
+                FingerprintImage.Visibility = Visibility.Collapsed;
+                return;
+            }
+
+            PersonImage.Visibility = Visibility.Visible;
+            PersonImage.Source = new BitmapImage(new Uri($"/Images/{number}-zdjecie.jpg", UriKind.Relative));
+
+            FingerprintImage.Visibility = Visibility.Visible;
+            FingerprintImage.Source = new BitmapImage(new Uri($"/Images/{number}-odcisk.jpg", UriKind.Relative));
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            string firstName = FirstNameTextBox.Text;
+            string lastName = LastNameTextBox.Text;
+
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
+            {
+                MessageBox.Show("Wprowadź dane");
+                return;
+            }
+
+            MessageBox.Show($"{firstName} {lastName} kolor oczu {_selectedEyeColor}");
+        }
+
+        private void EyeColorRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton radioButton = (RadioButton)sender;
+            _selectedEyeColor = (string)radioButton.Content;
+        }
+    }
+}
---
+++ inf04_2024_01_01/rozwiazanie/konsola/ConsoleApp/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        private static readonly int[] PositionWeights = new int[] { 1, 3, 7, 9 };
+
+        internal static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj numer PESEL:");
+            string peselNumber = Console.ReadLine() ?? string.Empty;
+            
+            string gender = ParseGender(peselNumber) == 'K'
+                ? "Kobieta"
+                : "Mężczyzna";
+            
+            bool isValid = IsPeselNumberValid(peselNumber);
+            
+            Console.WriteLine();
+            Console.WriteLine($"Płeć numeru PESEL to: {gender}");
+            Console.WriteLine($"Podany PESEL{(isValid ? "" : " nie")} jest poprawny");
+        }
+
+        /**********************************************
+        nazwa funkcji:       ParseGender
+        opis funkcji:        Wyciąga informacji o płci z numeru PESEL.
+        parametry:           peselNumber - string, ciąg znaków, numer pesel
+        zwracany typ i opis: char, płeć z numeru PESEL, 'K' dla kobiety, a 'M' dla mężczyzny
+        autor:               00000000000
+        ***********************************************/
+        private static char ParseGender(string peselNumber)
+        {
+            int digit = int.Parse(peselNumber[9].ToString());
+            return digit % 2 == 0 ? 'K' : 'M';
+        }
+
+        private static bool IsPeselNumberValid(string peselNumber)
+        {
+            int sum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                int weight = PositionWeights[i % PositionWeights.Length];
+                int digit = int.Parse(peselNumber[i].ToString());
+                sum += digit * weight;
+            }
+
+            int modulo = sum % 10;
+            int checksum = modulo == 0 ? 0 : 10 - modulo;
+            int lastDigit = int.Parse(peselNumber[10].ToString());
+            
+            return checksum == lastDigit;
+        }
+    }
+}
