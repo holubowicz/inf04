2f3dd7375016360c393072ffa6ccdd22271bce1f
Tymoteusz Hołubowicz <103210917+holubowicz@users.noreply.github.com>
2026-06-11 15:59:21 +0200

    feat: add solution for 2024_06_01 exam

---
+++ inf04_2024_06_01/rozwiazanie/konsolowa/ConsoleApp/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        private static readonly Random Rand = new Random();
+
+        internal static void Main(string[] args)
+        {
+            while (true)
+            {
+                int count = InputDiceCount();
+                int[] rolls = GenerateDiceRolls(count);
+                int score = CalculateScore(rolls);
+
+                PrintDiceRolls(rolls);
+                Console.WriteLine($"Liczba uzyskanych punktów: {score}");
+
+                Console.WriteLine("Jeszcze raz? (t/n)");
+                string input = (Console.ReadLine() ?? string.Empty).ToLower();
+                if (input == "n")
+                {
+                    break;
+                }
+            }
+        }
+
+        private static int InputDiceCount()
+        {
+            while (true)
+            {
+                Console.WriteLine("Ile kostek chcesz rzucić? (3 - 10)");
+                string input = Console.ReadLine() ?? string.Empty;
+                if (int.TryParse(input, out int number) && number >= 3 && number <= 10)
+                {
+                    return number;
+                }
+            }
+        }
+
+        /************************************************
+          nazwa:               GenerateDiceRolls
+          opis:                Generuje wyrzucenia kości, ilość kości jest podana jako argument.
+          parametry:           count - int, liczba całkowita, ilość kostek, które mają być "rzucone"
+          zwracany typ i opis: int[], tablica liczba całkowitych, wygenerowanie wyrzucenia kości
+          autor:               00000000000
+        ************************************************/
+        private static int[] GenerateDiceRolls(int count)
+        {
+            int[] rolls = new int[count];
+            for (int i = 0; i < count; i++)
+            {
+                rolls[i] = Rand.Next(1, 7);
+            }
+
+            return rolls;
+        }
+
+        private static int CalculateScore(int[] rolls)
+        {
+            int[] counts = new int[7];
+            foreach (int value in rolls)
+            {
+                counts[value]++;
+            }
+
+            int score = 0;
+            for (int i = 1; i <= 6; i++)
+            {
+                int count = counts[i];
+                if (count >= 2)
+                {
+                    score += i * count;
+                }
+            }
+
+            return score;
+        }
+
+        private static void PrintDiceRolls(int[] rolls)
+        {
+            for (int i = 0; i < rolls.Length; i++)
+            {
+                Console.WriteLine($"Kostka {i + 1}: {rolls[i]}");
+            }
+        }
+    }
+}
---
+++ inf04_2024_06_01/rozwiazanie/mobilna/MobileApp/MainPage.xaml.cs
@@ -0,0 +1,83 @@
+﻿namespace MobileApp;
+
+public partial class MainPage : ContentPage
+{
+    private static readonly int DiceCount = 5;
+    private static readonly Random Rand = new Random();
+
+    private int _score;
+    private int _gameScore;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void RollDiceButton_OnClicked(object? sender, EventArgs e)
+    {
+        int[] rolls = GenerateDiceRolls(DiceCount);
+
+        _score = CalculateScore(rolls);
+        _gameScore += _score;
+        UpdateScoreLabels();
+
+        for (int i = 0; i < DiceCount; i++)
+        {
+            Image image = (Image)ImagesStackLayout.Children[i];
+            int roll = rolls[i];
+
+            image.Source = ImageSource.FromFile("k" + roll + ".jpg");
+        }
+    }
+
+    private void ResetScoreButton_OnClicked(object? sender, EventArgs e)
+    {
+        _score = 0;
+        _gameScore = 0;
+        UpdateScoreLabels();
+
+        for (int i = 0; i < DiceCount; i++)
+        {
+            Image image = (Image)ImagesStackLayout.Children[i];
+            image.Source = ImageSource.FromFile("question.jpg");
+        }
+    }
+
+    private void UpdateScoreLabels()
+    {
+        ScoreLabel.Text = $"Wynik tego losowania: {_score}";
+        GameScoreLabel.Text = $"Wynik gry: {_gameScore}";
+    }
+
+    private int[] GenerateDiceRolls(int count)
+    {
+        int[] rolls = new int[count];
+        for (int i = 0; i < count; i++)
+        {
+            rolls[i] = Rand.Next(1, 7);
+        }
+
+        return rolls;
+    }
+
+    private int CalculateScore(int[] rolls)
+    {
+        int[] counts = new int[7];
+        foreach (int value in rolls)
+        {
+            counts[value]++;
+        }
+
+        int score = 0;
+        for (int i = 1; i <= 6; i++)
+        {
+            int count = counts[i];
+            if (count >= 2)
+            {
+                score += i * count;
+            }
+        }
+
+        return score;
+    }
+}
