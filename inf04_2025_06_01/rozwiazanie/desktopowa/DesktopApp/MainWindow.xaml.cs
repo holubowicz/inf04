fd868ac8d2a7d507b5d18d6a169b18ffcdda7ef1
Tymoteusz Hołubowicz <103210917+holubowicz@users.noreply.github.com>
2026-06-13 13:23:03 +0200

    feat: add solution for 2025_06_01 exam

---
+++ inf04_2025_06_01/rozwiazanie/desktopowa/DesktopApp/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private byte _red = 255;
+        private byte _green = 255;
+        private byte _blue = 255;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RedColorSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (RedColorLabel == null) return;
+
+            _red = (byte)e.NewValue;
+            RedColorLabel.Content = _red;
+
+            UpdateColorRectangle();
+        }
+
+        private void GreenColorSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (GreenColorLabel == null) return;
+
+            _green = (byte)e.NewValue;
+            GreenColorLabel.Content = _green;
+
+            UpdateColorRectangle();
+        }
+
+        private void BlueColorSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (BlueColorLabel == null) return;
+
+            _blue = (byte)e.NewValue;
+            BlueColorLabel.Content = _blue;
+
+            UpdateColorRectangle();
+        }
+
+        private void SaveColorButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveColorLabel.Content = string.Join(", ", _red, _green, _blue);
+            SaveColorLabel.Background = new SolidColorBrush(Color.FromRgb(_red, _green, _blue));
+        }
+
+        private void UpdateColorRectangle()
+        {
+            ColorRectangle.Fill = new SolidColorBrush(Color.FromRgb(_red, _green, _blue));
+        }
+    }
+}
---
+++ inf04_2025_06_01/rozwiazanie/konsolowa/ConsoleApp/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        private static readonly int ZakresMinimum = 1;
+        private static readonly int ZakresMaksimum = 49;
+        private static readonly int LosowanieIloscLiczb = 6;
+
+        private static readonly Random Rand = new Random();
+
+        internal static void Main(string[] args)
+        {
+            int ilosc;
+            while (true)
+            {
+                Console.WriteLine("Ile wygenerować losowań?");
+                if (int.TryParse(Console.ReadLine() ?? string.Empty, out ilosc) && ilosc > 0)
+                {
+                    break;
+                }
+            }
+
+            int[,] losowania = WygenerujLosowania(ilosc);
+            WyswietlWyniki(losowania);
+        }
+
+        /**********************************************
+        nazwa funkcji:       WygenerujLosowania
+        opis funkcji:        Generuje losowania liczb pseudolosowych, ilość losowań jest determinowane podawanym argumentem funkcji.
+        parametry:           ilosc - int, liczba całkowita, ilość losowań, które mają zostać wykonane
+        zwracany typ i opis: int[,] - dwuwymiarowa tablica liczb całkowitych, przechowuje wyniki kilku losowań
+        autor:               00000000000
+        ***********************************************/
+        private static int[,] WygenerujLosowania(int ilosc)
+        {
+            int[,] losowania = new int[ilosc, LosowanieIloscLiczb];
+
+            for (int i = 0; i < ilosc; i++)
+            {
+                HashSet<int> wiersz = new HashSet<int>();
+                while (wiersz.Count != LosowanieIloscLiczb)
+                {
+                    wiersz.Add(Rand.Next(ZakresMinimum, ZakresMaksimum + 1));
+                }
+
+                for (int j = 0; j < LosowanieIloscLiczb; j++)
+                {
+                    losowania[i, j] = wiersz.ElementAt(j);
+                }
+            }
+
+            return losowania;
+        }
+
+        private static void WyswietlWyniki(int[,] losowania)
+        {
+            Console.WriteLine("Zestawy wylosowanych liczb:");
+
+            for (int i = 0; i < losowania.GetLength(0); i++)
+            {
+                Console.Write($"Losowanie {i + 1}:");
+                for (int j = 0; j < LosowanieIloscLiczb; j++)
+                {
+                    Console.Write($" {losowania[i, j]}");
+                }
+
+                Console.WriteLine();
+            }
+
+            int[] wystapienia = new int[ZakresMaksimum + 1];
+            foreach (int liczba in losowania)
+            {
+                wystapienia[liczba]++;
+            }
+
+            for (int i = ZakresMinimum; i <= ZakresMaksimum; i++)
+            {
+                Console.WriteLine($"Wystąpienia liczby {i}: {wystapienia[i]}");
+            }
+        }
+    }
+}
